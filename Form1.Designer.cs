0ddc5545fe2b3336fd319f9eea867b47a3233710
venyraa <167503345+venyraa@users.noreply.github.com>
2024-11-08 17:48:25 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿using UTS;
+
+namespace UTS_PBO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(58, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 25);
+            label1.TabIndex = 0;
+            label1.Text = "username";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(206, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(252, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(58, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 25);
+            label2.TabIndex = 2;
+            label2.Text = "password";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.ForeColor = SystemColors.InactiveCaptionText;
+            textBox2.Location = new Point(206, 126);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(252, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(545, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 4;
+            button1.Text = "masuk";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RosyBrown;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            ForeColor = Color.Black;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1; 
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
+
+namespace UTS PBO;
+{
+    public partial class Form1 : Form
+{
+    private string username;
+    private string password;
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        string username = textBox1.Text;
+        string password = textBox2.Text;
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            MessageBox.Show("Masukkan username dan password terlebih dahulu");
+        }
+        else
+        {
+            var loginSuccess = C_Login.login(username, password);
+
+            if (loginSuccess == 1)
+            {
+                MessageBox.Show("Login berhasil!");
+                // Tambahkan logika setelah login berhasil, seperti membuka form baru
+            }
+            else if (loginSuccess == 0)
+            {
+                MessageBox.Show("Username atau password salah!");
+            }
+        }
+    }
+}
